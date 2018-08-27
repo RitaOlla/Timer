c90fa06a9f164eefd57db5607d6b72abff7a0308
Olla <238052@student.pwr.edu.pl>
2018-08-27 17:24:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rita_ola/shareaction/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.rita_ola.shareaction;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
+import android.widget.Toast;
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button controllerButton;
+    Boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer() {
+
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        controllerButton.setText("Go!");
+        timerSeekBar.setEnabled(true);
+        counterIsActive = false;
+
+    }
+
+    public void updateTimer(int secondsLeft) {
+
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9) {
+
+            secondString = "0" + secondString;
+
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+    }
+
+
+    public void controlTimer(View view) {
+
+        if (counterIsActive == false) {
+
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Stop");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    resetTimer();
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.kukuryku);
+                    mplayer.start();
+
+                }
+            }.start();
+
+        } else {
+
+            resetTimer();
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        controllerButton = (Button) findViewById(R.id.controllerButton);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                updateTimer(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+
+        switch (item.getItemId()) {
+
+            case R.id.mShare:
+
+                Intent i = new Intent(
+
+                        android.content.Intent.ACTION_SEND);
+
+                i.setType("text/plain");
+
+                startActivity(Intent.createChooser(
+
+                        i,
+
+                        "Share Via"));
+
+                break;
+
+        }
+
+
+        Toast.makeText(getApplicationContext(), "Share", Toast.LENGTH_SHORT).show();
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
